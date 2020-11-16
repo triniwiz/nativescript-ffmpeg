c3ec67b93922fd18fa2d1bccd2d00fdc2ad7cad5
Peter Chapman <ops@crowers.co.uk>
2020-11-16 21:00:38 +0000

    Build 7.0.4 Updated more to NS7

--- src/references.d.ts
+++ src/references.d.ts
@@ -1,4 +1,4 @@
-/// <reference path="./node_modules/@nativescript/types/ios.d.ts" />
-/// <reference path="./node_modules/@nativescript/types/android.d.ts" />
+/// <reference path="./node_modules/@nativescript/types-ios/index.d.ts" />
+/// <reference path="./node_modules/@nativescript/types-android/index.d.ts" />
 /// <reference path="./typings/android-mobileFFmpeg.d.ts" />
 /// <reference path="./typings/android.d.ts" />
